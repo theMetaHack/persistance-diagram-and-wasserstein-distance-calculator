18f3f9178522c6b6cd99f0b586bf30baa0379537
theMetaHack <65082506+theMetaHack@users.noreply.github.com>
2023-07-21 22:57:50 -0400

    Create PD.py

---
+++ PD.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
+from ripser import Rips
+
+# Load the image
+img_file_path = "D:/data/topoFolder/trainingFolder/topoTrain/IMG-0001-00067.jpg"
+image = plt.imread(img_file_path)
+
+# Convert the image to grayscale
+image = np.mean(image, axis=2)
+
+# Compute the persistence diagram
+rips = Rips()
+diagrams = rips.fit_transform(image)
+
+# Plot the persistence diagram
+rips.plot(diagrams)
+
+# Save the persistence diagram as a JPEG
+# fig.savefig("persistence_diagram.jpg", format="jpg", dpi=300)
